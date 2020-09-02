f8d43b7f47d64aa0d93e84ae04c90d0252f79a7d
Tahmeed Tarek <tahmeedtarek@gmail.com>
2020-09-03 00:21:31 +0600

    models - content, keywords

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from faker import Faker
+from random import choice, randint
 
-# Create your models here.
+faker = Faker('en_US')
+
+
+class Content(models.Model):
+
+    text = models.TextField('text')
+    upload_datetime = models.DateTimeField('upload datetime')
+
+    def save(self, *args, **kwargs):
+        if not self.id and not self.upload_datetime:
+            self.upload_datetime = timezone.now()
+        return super().save(*args, **kwargs)
+
+
+class Keyword(models.Model):
+
+    word = models.CharField('key word', max_length=128)
+    content = models.ForeignKey(Content, related_name='keywords', on_delete=models.CASCADE)
+    weight = models.DecimalField('unit price', decimal_places=3, max_digits=5)
