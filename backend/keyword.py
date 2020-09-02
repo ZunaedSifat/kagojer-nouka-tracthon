26503be427ed0d38ccad61a46201fbdbf3967845
ashiqur.superfly <ashiqur.buet16@gmail.com>
2020-09-03 05:18:20 +0600

    refactor

--- backend/keyword.py
+++ backend/keyword.py
@@ -1,10 +1,11 @@
 import nltk
 from summa import keywords
 from nltk.corpus import stopwords
-from nltk.tag import pos_tag, pos_tag_sents
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
-
-# from nltk.tokenize import sent_tokenize, word_tokenize
+from string import punctuation
+from nltk.corpus import wordnet as wn
 
 '''
 Utils
@@ -13,12 +14,20 @@ def read_file(file_name):
     file1 = open(file_name, "r", encoding='UTF-8')
     return file1.read()
 
-def sort_based_on_weight(words):
+'''
+returns a list of tuples (word, weight)
+'''
+def sort_based_on_weight(words: dict):
     return sorted(words.items(), key=lambda x: x[1], reverse=True)
 
+def remove_leading_and_trailing_special_chars(s: str):
+    s = s.replace('“', "") # replacing weird unicode character (u8220) that often comes out in articles
+    s = s.replace('‘', "") # replacing weird unicode character (u8216) that often comes out in articles
+
+    return s.strip(punctuation)
 
 '''
-Nltk:
+nltk:
 All functions except init_nltk() returns a dictionary where key==word_string and value==weight_float
 '''
 def init_nltk():
@@ -26,16 +35,34 @@ def init_nltk():
     nltk.download('wordnet')
     nltk.download('words')
     nltk.download('averaged_perceptron_tagger')
-    nltk.download('maxent_ne_chunker')
+    nltk.download('punkt')
+    # nltk.download('maxent_ne_chunker')
 
+
+'''
+nltk multi-pass utils
+'''
 def remove_stopwords(input_words: dict):
     words = dict()
     stop_words = set(stopwords.words("english"))
+    # print('StopWords:\n', stop_words)
+
     for k,v in input_words.items():
-        if k in stop_words:
-            print('Removed StopWord', k)
+        lowered = str(k).lower()
+
+        contains_stop_word = False
+
+        for item in stop_words:
+            sw = str(item)
+            if (sw.__len__() >= 4 and lowered.find(sw) != -1) or (lowered.__len__() >= 4 and sw.find(lowered) != -1) or (lowered == sw):
+                contains_stop_word = True
+                # print('(removed,responsible-sw)', lowered, ":", sw)
+                break
+
+        if contains_stop_word:
+            pass
         else:
-            words[k]=v
+            words[k] = v
     return words
 
 
@@ -47,13 +74,16 @@ def lemmatize(words: dict):
 
     for k in words:
         # lemmatized = lem.lemmatize(k, 'v')
+        # ps = nltk.PorterStemmer()
         lemmatized = lem.lemmatize(k)
         if lemmatized not in lemmatized_words:
             lemmatized_words[lemmatized] = words.get(k)
 
     return lemmatized_words
 
-
+'''
+nltk single-pass utils
+'''
 def generate_summa_keywords(text):
     _keywords = list()
     try:
@@ -64,35 +94,56 @@ def generate_summa_keywords(text):
     return _keywords
 
 
-def find_proper_nouns(text):
+def find_unique_proper_nouns(text):
+    tokenized = sent_tokenize(text)
+    stop_words = set(stopwords.words("english"))
+    parts_of_speech_tagged_words = list()
+    for i in tokenized:
+        wordsList = nltk.word_tokenize(i)
+        wordsList = [w for w in wordsList if not w in stop_words]
+        parts_of_speech_tagged_words = parts_of_speech_tagged_words + pos_tag(nltk.word_tokenize(i))
 
-    parts_of_speech_tagged_words = pos_tag(text.split())
-    print('\n', parts_of_speech_tagged_words, '\n')
+    # print('pos-tagged:\n', parts_of_speech_tagged_words, '\n')
     proper_nouns = [word for word,pos in parts_of_speech_tagged_words if pos == 'NNP']
 
-    # remove
-    # for item in proper_nouns:
-    #     if str(item[0]).isalnum():
+    result = dict()
+    for item in proper_nouns:
+        upper_cased = remove_leading_and_trailing_special_chars(str(item)).upper()
 
-    return proper_nouns
+        if upper_cased.__len__() < 3: continue
+        if result.get(upper_cased) is None:
+            result[upper_cased] = 1
+        else:
+            result[upper_cased] = result[upper_cased] + 1
+
+    return result
 
 
 def process(text):
     raw_keywords = generate_summa_keywords(text)
-    print('Summa Generated Keywords: Total:', raw_keywords.__len__(), '\n', raw_keywords)
+    # print('Summa Generated Keywords: Total:', raw_keywords.__len__(), '\n', raw_keywords)
 
     l = lemmatize(raw_keywords)
-    print('Lemmatized Keywords: Total:', l.__len__(), '\n', l)
+    # print('Lemmatized Keywords: Total:', l.__len__(), '\n', l)
 
-    after_removing_stopwords = remove_stopwords(l)
-    print('stop words removed keywords: Total:', after_removing_stopwords.__len__(), '\n', after_removing_stopwords)
+    keywords_after_removing_stopwords = remove_stopwords(l)
+    # print('stop words removed keywords: Total:', keywords_after_removing_stopwords.__len__(), '\n', keywords_after_removing_stopwords)
 
-    proper_nouns = find_proper_nouns(text)
-    print(proper_nouns)
+    sorted_keywords = sort_based_on_weight(keywords_after_removing_stopwords)
+    print('KEYWORDS:\n', sorted_keywords)
+
+    proper_nouns = find_unique_proper_nouns(text)
+    proper_nouns_after_removing_stop_words = remove_stopwords(proper_nouns)
+    lemmatized_proper_nouns = lemmatize(proper_nouns_after_removing_stop_words)
+
+    sorted_nouns = sort_based_on_weight(lemmatized_proper_nouns)
+    print('NOUNS:\n', sorted_nouns)
+
+    return sorted_nouns + sorted_keywords
 
 
-init_nltk()
-process(read_file('input.txt'))
+# init_nltk()
+# process(read_file('input.txt'))
 
 
 # TODO Keyword:
@@ -102,9 +153,11 @@ process(read_file('input.txt'))
 # idea: filter out keywords that are not nouns?
 
 # TODO Proper Noun:
-# find proper nouns
-# remove special characters at the beginning or end of proper nouns
-# give them weight based on count
+# find distinct proper nouns : DONE
+# give them weight based on count : DONE
+# lemmatize proper nouns : DONE
+# remove stop_words from proper nouns : DONE
+# idea: filter out words that have other possible POS e.g: find all possible POS of a word then pick only those words that are pure noun
 
 # TODO Union Both Results:
 # weight of proper nouns = max_weight of keywords * count
