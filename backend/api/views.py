d9e41dea6baf5caf3a4b1027b8308d73956e832b
Tahmeed Tarek <tahmeedtarek@gmail.com>
2020-09-03 16:16:13 +0600

    now availabe with time!

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django.db.models import Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -8,6 +10,22 @@ from api.models import Keyword
 @api_view(http_method_names=['GET'])
 def trending_keywords(request):
 
-    keywords = Keyword.objects.values('word').annotate(count=Count('word')).order_by('-count')
+    period = request.GET.get('period')
+    count = request.GET.get('count')
+
+    keywords = Keyword.objects.all()
+    end = datetime.now()
+
+    if period == 'days':
+        start = end - timedelta(days=int(count))
+        keywords = keywords.filter(content__upload_datetime__gte=start, content__upload_datetime__lte=end)
+    elif period == 'weeks':
+        start = end - timedelta(weeks=int(count))
+        keywords = keywords.filter(content__upload_datetime__gte=start, content__upload_datetime__lte=end)
+    else:
+        # all-time
+        pass
+
+    keywords = keywords.values('word').annotate(count=Count('word')).order_by('-count')
 
     return Response(keywords)
